3ba6620add2f7e66eec391a56bb04c14415e67b6
ElodieSavignard <elodie.savignard@outlook.fr>
2024-09-18 22:50:18 +0200

    First commit

---
+++ Youtube/Code/full_youtube_fetch.py
@@ -0,0 +1,108 @@
+import os
+import googleapiclient.discovery
+import pandas as pd
+import sys
+from datetime import datetime
+from googleapiclient.discovery import build
+
+
+
+def get_channel_id(api_key, channel_name):    
+    request = youtube.search().list(
+        q=channel_name,
+        part='snippet',
+        type='channel',
+        maxResults=1
+    )
+    response = request.execute()
+    
+    if response['items']:
+        channel_id = response['items'][0]['snippet']['channelId']
+        return channel_id
+    else:
+        return None
+                                                                                
+
+def get_channel_videos(channel_id):
+    request = youtube.channels().list(
+        part='contentDetails',
+        id=channel_id
+    )
+    response = request.execute()
+    uploads_playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    videos = []
+    next_page_token = None
+
+    while True:
+        playlist_request = youtube.playlistItems().list(
+            part='snippet',
+            playlistId=uploads_playlist_id,
+            maxResults=50,
+            pageToken=next_page_token
+        )
+        playlist_response = playlist_request.execute()
+
+        for item in playlist_response['items']:
+            video_id = item['snippet']['resourceId']['videoId']
+            video_title = item['snippet']['title']
+            video_published_at = item['snippet']['publishedAt']
+        
+            video_request = youtube.videos().list(
+                part='statistics,contentDetails',
+                id=video_id
+            )
+            video_response = video_request.execute()
+            video_duration_ytb = video_response['items'][0]['contentDetails']['duration']
+            video_duration = video_duration_ytb[2:]
+            try:
+                view_count = video_response['items'][0]['statistics']['viewCount']
+            except KeyError:
+                view_count = "No views"
+            try:
+                video_like = video_response['items'][0]['statistics']['likeCount']
+            except KeyError:
+                video_like = "No likes"
+            try:
+                video_comment = video_response['items'][0]['statistics']['commentCount']
+            except KeyError:
+                video_comment = "Comments have been disabled"
+        
+            
+            videos.append({
+                'title': video_title,
+                'published at': video_published_at,
+                'views count': view_count,
+                'duration': video_duration,
+                'likes': video_like,
+                'comments': video_comment
+            })
+
+        next_page_token = playlist_response.get('nextPageToken')
+        if next_page_token is None:
+            break
+
+    return videos
+
+def save_to_excel(videos, channel_name):
+    today_date = datetime.now().strftime("%Y-%m-%d")
+    filename = f"{channel_name}_{today_date}.xlsx"
+    df = pd.DataFrame(videos)
+    df.to_excel('/Results/{filename}', index=False)
+    print(f"The data has been saved in the file {filename} inside the folder Results")
+
+
+
+if __name__ == "__main__":
+    API_KEY = 'AIzaSyC8VRoeokncs5zdKEo03C4J4rPgbox-HgA'
+    channel_name = sys.argv[1]
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+    channel_id = get_channel_id(API_KEY, channel_name)
+    if channel_id:
+        print(f"The channel ID '{channel_name}' is : {channel_id}")
+    else:
+        print(f"No channel found with the name '{channel_name}'.")
+
+    videos = get_channel_videos(channel_id)
+    save_to_excel(videos, channel_name)
---
+++ Youtube/Code/readme.md
@@ -0,0 +1,24 @@
+### Create Google API Key
+
+To create your application's API key:
+
+1 - Go to the [API Console](https://console.cloud.google.com/welcome?project=scrapcomplo).\
+2 - From the projects list, select a project or create a new one.\
+3 - If the APIs & services page isn't already open, open the left side menu and select APIs & services.\
+4 - On the left, choose Credentials.\
+5 - Click Create credentials and then select API key.
+6 - Inside the searchbar find "Youtube Data API V3", click and enable it.
+
+https://support.google.com/googleapi/answer/6158862?hl=en
+
+### Add your API Key inside script
+
+Insert your API key inside var `API_KEY = ''` at line 100 on the full_youtube_fetch.py script.
+
+### Install packages
+Run the command inside your terminal :\
+```pip install google-api-python-client openpyxl pandas```
+
+
+### Run Script
+python3.9 full_youtube_fetch.py ChannelName
